dbb3e0f9bac364a81cc108151c42e9b6944dc6b6
Kiran-Akkaluri <kiranreddyakkaluri555@gmail.com>
2026-03-13 10:17:59 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,24 @@
+<?php
+
+include "db.php";
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$dob = $_POST['dob'];
+$department = $_POST['department'];
+$phone = $_POST['phone'];
+
+$sql = "INSERT INTO students(name,email,dob,department,phone)
+VALUES('$name','$email','$dob','$department','$phone')";
+
+if($conn->query($sql)==TRUE){
+    echo "Student Registered Successfully <br>";
+    echo "<a href='index.html'>Back</a>";
+}
+else{
+    echo "Error: ".$conn->error;
+}
+
+$conn->close();
+
+?>
